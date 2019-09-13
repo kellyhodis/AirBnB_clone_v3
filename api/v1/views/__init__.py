dc966d95fbdd3afabe5197f1d33e51356c72c6c5
Kelly Hodis <668@holbertonschool.com>
2019-09-13 01:46:53 +0000

    Update imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,4 +11,4 @@ from api.v1.views import cities
 from api.v1.views import amenities
 from api.v1.views import users
 from api.v1.views import places
-from api.v1.views import reviews
+from api.v1.views import places_reviews
